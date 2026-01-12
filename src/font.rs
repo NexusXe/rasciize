014def9451aca82ad2ef0b209c4784aab5f9260a
NexusXe <andastrike@gmail.com>
2026-01-11 18:14:22 -0600

    formatting

--- src/font.rs
+++ src/font.rs
@@ -12,21 +12,21 @@ pub type IntensityMap<V> = BTreeMap<OrderedFloat<FloatPrecision>, V>;
 #[allow(unused)]
 #[inline(always)]
 fn glyph_is_safe(input: u16) -> bool {
-// #define	isalpha(c)	(_ctype[(unsigned char)(c)]&(_U|_L))
-// #define	isupper(c)	(_ctype[(unsigned char)(c)]&_U)
-// #define	islower(c)	(_ctype[(unsigned char)(c)]&_L)
-// #define	isdigit(c)	(_ctype[(unsigned char)(c)]&_N)
-// #define	isxdigit(c)	(_ctype[(unsigned char)(c)]&_X)
-// #define	isspace(c)	(_ctype[(unsigned char)(c)]&_S)
-// #define	ispunct(c)	(_ctype[(unsigned char)(c)]&_P)
-// #define	isalnum(c)	(_ctype[(unsigned char)(c)]&(_U|_L|_N))
-// #define	isprint(c)	(_ctype[(unsigned char)(c)]&(_P|_U|_L|_N|_B))
-// #define	isgraph(c)	(_ctype[(unsigned char)(c)]&(_P|_U|_L|_N))
-// #define	iscntrl(c)	(_ctype[(unsigned char)(c)]&_C)
-// #define	isascii(c)	((unsigned char)(c)<=0177)
-// #define	_toupper(c)	((c)-'a'+'A')
-// #define	_tolower(c)	((c)-'A'+'a')
-// #define	toascii(c)	((c)&0177)
+    // #define	isalpha(c)	(_ctype[(unsigned char)(c)]&(_U|_L))
+    // #define	isupper(c)	(_ctype[(unsigned char)(c)]&_U)
+    // #define	islower(c)	(_ctype[(unsigned char)(c)]&_L)
+    // #define	isdigit(c)	(_ctype[(unsigned char)(c)]&_N)
+    // #define	isxdigit(c)	(_ctype[(unsigned char)(c)]&_X)
+    // #define	isspace(c)	(_ctype[(unsigned char)(c)]&_S)
+    // #define	ispunct(c)	(_ctype[(unsigned char)(c)]&_P)
+    // #define	isalnum(c)	(_ctype[(unsigned char)(c)]&(_U|_L|_N))
+    // #define	isprint(c)	(_ctype[(unsigned char)(c)]&(_P|_U|_L|_N|_B))
+    // #define	isgraph(c)	(_ctype[(unsigned char)(c)]&(_P|_U|_L|_N))
+    // #define	iscntrl(c)	(_ctype[(unsigned char)(c)]&_C)
+    // #define	isascii(c)	((unsigned char)(c)<=0177)
+    // #define	_toupper(c)	((c)-'a'+'A')
+    // #define	_tolower(c)	((c)-'A'+'a')
+    // #define	toascii(c)	((c)&0177)
     const U: u16 = 0x01;
     const L: u16 = 0x02;
     const N: u16 = 0x04;
@@ -121,7 +121,12 @@ pub fn get_coverage_array(
 }
 
 #[inline]
-pub fn get_intensity(font: &FontRef, font_size: f32, glyph_id: GlyphId, glyph_char: char) -> Option<FloatPrecision> {
+pub fn get_intensity(
+    font: &FontRef,
+    font_size: f32,
+    glyph_id: GlyphId,
+    glyph_char: char,
+) -> Option<FloatPrecision> {
     let outlined_glyph: OutlinedGlyph =
         font.outline_glyph(glyph_id.with_scale(PxScale::from(font_size)))?;
     let bounds = outlined_glyph.px_bounds();
@@ -141,7 +146,8 @@ pub fn get_intensity(font: &FontRef, font_size: f32, glyph_id: GlyphId, glyph_ch
         if unsafe {
             fsub_fast(bounds.width(), standard_glyph_width_wide).abs() > font_size.sqrt()
                 && (fsub_fast(bounds.width(), standard_glyph_width_narrow).abs() > font_size.sqrt())
-        } && !glyph_is_safe(glyph_char as u16) {
+        } && !glyph_is_safe(glyph_char as u16)
+        {
             return None;
         }
     }
